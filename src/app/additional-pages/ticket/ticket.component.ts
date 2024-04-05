ae1334cfe6c680ecf93ea5f7bdbe6d4e21f75499
Andó Attila <andoattila698@gmail.com>
2024-04-05 15:12:26 +0200

    Projekt Munka

---
+++ src/app/additional-pages/ticket/ticket.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
+import { NgxSpinnerService } from 'ngx-spinner';
+export interface Element {
+  name: string;
+  position: number;
+  discount: string;
+  price: number;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: 1, name: 'Felnőtt', discount: 'Nincs', price: 3600 },
+  {position: 2, name: 'Diák', discount: 'Van', price: 2400},
+  {position: 3, name: 'Nyugdíjas', discount: 'Van', price: 2400},
+  {position: 4, name: 'Gyerek,', discount: 'Ingyenes', price: 0},
+  
+];
+
+
+@Component({
+  selector: 'app-ticket',
+  templateUrl: './ticket.component.html',
+  styleUrls: ['./ticket.component.css']
+})
+export class TicketComponent {
+  displayedColumns: string[] = ['demo-position', 'demo-name', 'demo-discount','demo-price'];
+  dataSource = ELEMENT_DATA;
+  constructor(private spinner:NgxSpinnerService){}
+  openSpinner(){
+    this.spinner.show();
+    setTimeout(()=>{
+      this.spinner.hide();
+    }, 7500)
+  }
+}
---
+++ src/app/sharepage/navbar/navbar.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  lang:string ='';
+
+  currentUser:any
+  SAdmin=false
+
+ngOnInit(): void {
+  
+
+  this.lang = localStorage.getItem('lang') ||'hu';
+}
+
+  constructor(private auth:AuthService, private router:Router,private translateService:TranslateService,private spinner:NgxSpinnerService){
+    this.auth.getCurrentUser().subscribe(
+      (user)=>this.currentUser=user
+    )
+    this.auth.SadminSub.subscribe(
+      (res)=> this.SAdmin=res
+    )
+  }
+
+  logout(){
+    this.auth.logout()
+    this.router.navigate(['/signin'])
+  }
+  ChangeLanguage(lang:any){
+    const selectedLanguage = lang.target.value;
+
+    localStorage.setItem('lang',selectedLanguage);
+
+    this.translateService.use(selectedLanguage);
+  }
+  openSpinner(){
+    this.spinner.show();
+    setTimeout(()=>{
+      this.spinner.hide();
+    }, 7500)
+  }
+}
